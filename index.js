e294d52efcd30eebc4680e85f29c4930b5d9dde6
Xavier Dunkley <xavierxv18@gmail.com>
2022-02-01 14:48:07 -0500

    commit tree 4c325dad42d78dcd634fe34de1a77e34e8b50700

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location){
+    const headQuarters = 42;
+    if(location>=headQuarters){
+        return location-headQuarters;
+    }else{
+        return headQuarters-location;
+    }
+}
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location)*264;
+}
+
+function distanceTravelledInFeet(startingPoint,endingPoint){
+    if(startingPoint>endingPoint){
+        return (startingPoint-endingPoint)*264;
+    }else{
+        return (endingPoint-startingPoint)*264;
+    }
+}
+
+function calculatesFarePrice(startingPoint,endingPoint){
+    let distance = distanceTravelledInFeet(startingPoint,endingPoint);
+    if(distance<=400){
+        return 0;
+    }else if(distance>400 && distance<=2000){
+        return (distance-400)*0.02;
+    }else if(distance>2000 && distance<=2500){
+        return 25;
+    }else if(distance>2500){
+        return 'cannot travel that far';
+    }
+}
